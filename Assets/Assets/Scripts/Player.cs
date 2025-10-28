b50170bc34075f777216b09e476fc4ff753a8fd3
palkull <palkulx2000@gmail.com>
2025-10-28 10:14:04 -0600

    Implement player movement control and add scene template settings

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float entradaHorizontal;
+    [SerializeField] private float velocidadMovimiento = 5f;
+
+    private void Awake()
     {
-        
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        entradaHorizontal = Input.GetAxisRaw("Horizontal");
     }
+    private void FixedUpdate()
+    {
+        ControlarMovimientoHorizontal();
+
+    }
+
+    private void ControlarMovimientoHorizontal()
+    {
+        rb.linearVelocity = new Vector2(entradaHorizontal * velocidadMovimiento, rb.linearVelocity.y);
+
+        if ((entradaHorizontal > 0 && !MirandoDerecha()) || (entradaHorizontal < 0 && MirandoDerecha()))
+        {
+            Girar();
+        }
+    }
+
+
+    private void Girar()
+    {
+        Vector3 escala = transform.localScale;
+        escala.x *= -1;
+        transform.localScale = escala;
+    }
+    private bool MirandoDerecha()
+    {
+        return transform.localScale.x == 1;
+    }
+
+
 }
