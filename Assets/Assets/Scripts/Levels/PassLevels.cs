1d90eed1e91640183981df412542c5488c13eb87
palkull <palkulx2000@gmail.com>
2025-11-29 20:25:52 -0600

    Se añadio salto de nivel

---
+++ Assets/Assets/Scripts/Levels/PassLevels.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassLevels : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+    }
+}
